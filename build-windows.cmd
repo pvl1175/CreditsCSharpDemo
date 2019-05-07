e44a2d63451c5476013b5564a6948b0c2e428ba0
pvl1175 <vitaly.popov.75@gmail.com>
2019-05-07 13:15:52 +0300

    update

--- build-windows.cmd
+++ build-windows.cmd
@@ -3,3 +3,4 @@ thrift -gen csharp -out . .\cs-api\general.thrift
 thrift -gen csharp -out . .\cs-api\api.thrift
 dotnet add package apache-thrift-netcore --version 0.9.3.2
 dotnet add package SimpleBase --version 1.8.0
+pause
---
+++ clean.cmd
@@ -0,0 +1,10 @@
+rmdir /S /Q bin
+rmdir /S /Q CS-API
+rmdir /S /Q obj
+rmdir /S /Q NodeApi
+del Annotation.cs
+del APIResponse.cs
+del ByteCodeObject.cs
+del MethodArgument.cs
+del MethodDescription.cs
+del Variant.cs
