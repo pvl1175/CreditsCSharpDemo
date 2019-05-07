4d82dc8fcbb4a03b797808f4f1e5496e6fdb77d5
pvl1175 <vitaly.popov.75@gmail.com>
2019-05-07 13:28:48 +0300

    update

--- build-linux.sh
+++ build-linux.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/CREDITSCOM/CS-API
-thrift -gen csharp -out . .\cs-api\general.thrift
-thrift -gen csharp -out . .\cs-api\api.thrift
+thrift -gen csharp -out . ./CS-API/general.thrift
+thrift -gen csharp -out . ./CS-API/api.thrift
 dotnet add package apache-thrift-netcore --version 0.9.3.2
 dotnet add package SimpleBase --version 1.8.0
